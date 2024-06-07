b6e61fea5b92aebb29a3d262362f9a22c5ca36fd
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-06-07 16:49:05 -0300

    spinner on loading

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,3 +1,5 @@
 export default function Spinner() {
-  return <div className="spinner" />;
+  return (
+    <div className="animate-spinner absolute left-[50%] top-1/3 h-[85px] w-[85px] rounded-[50%] border-b-[5px] border-l-[5px] border-r-[5px] border-t-[5px] border-b-[#e2e7e9] border-l-[#40555e] border-r-[#e2e7e9] border-t-[#e2e7e9]" />
+  );
 }
